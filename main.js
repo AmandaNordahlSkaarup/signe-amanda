7384fc6e7d067174e32820f4f2151c40d5dea57f
Amanda <nordahl96@hotmail.com>
2021-09-30 11:46:37 +0200

    bla

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+let di1 = document.querySelector(".div1")
+di1.addEventListener('click', function(){
+    alert("I'm peach!")
+});
+let di2 = document.querySelector(".div2")
+di2.addEventListener('click', function(){
+    alert("I'm blue!")
+});
+let di3 = document.querySelector(".div3")
+di3.addEventListener('click', function(){
+    alert("I'm green!")
+});
+
+
+
+
+
+/*
 function mouseDown() {
     document.getElementsByClassName("div1").style.color = "blue";
   }
@@ -12,4 +30,4 @@ function mouseDown() {
 
   function mouseout() {
       document.getElementsByClassName("div2").style.display = "block";
-  }
+  } */
