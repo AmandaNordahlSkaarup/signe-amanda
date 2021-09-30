85b56ca7280cbdb92aa2d5574a8a765590a879b0
Amanda <nordahl96@hotmail.com>
2021-09-30 11:23:40 +0200

    no
    no

---
+++ main.js
@@ -0,0 +1,15 @@
+function mouseDown() {
+    document.getElementsByClassName("div1").style.color = "blue";
+  }
+
+  function mouseUp() {
+    document.getElementsByClassName("div1").style.color = "red";
+  }
+
+  function mouseover() {
+      document.getElementsByClassName("div2").style.display = "none";
+  }
+
+  function mouseout() {
+      document.getElementsByClassName("div2").style.display = "block";
+  }
